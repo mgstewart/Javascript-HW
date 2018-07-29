4327381b8c21f106ff0214df71ccf41ecc3b78db
mike.stewartNU <septober1@gmail.com>
2018-07-29 01:16:40 -0500

    Fixed bug with table update

--- static/js/app.js
+++ static/js/app.js
@@ -25,42 +25,73 @@ function monitor_shape() {
    var shape_select = d3.select("#shape-select");
    var options = shape_select._groups[0][0]
    //console.log(options.value);
+   return options.value
 };
 
 // Define helper function to gather all text entries
 function retrieve_inputs() {
     var inputs = d3.selectAll("input");
-    var inputs = inputs._groups[0]
+    var inputs = inputs._groups[0];
+    var input_obj = {};
     inputs.forEach(input => {
-        
+    // Ensure inputs are converted to lower case to match data in data.js
+    input_obj[String(input.id)] = input.value.toLowerCase();
     });
-    console.log(inputs);
+    //console.log(input_obj);
+    return input_obj;
 };
+
+// Define helper function to drop the previous table values
+function clear_table() {
+    var temp_tr = d3.selectAll(".temp-tr").remove();
+}
 
 filterTable.on("submit", function() {
   // Prevent the page from refreshing
   d3.event.preventDefault();
-  monitor_shape();
-  retrieve_inputs();
-  // Use D3 to select all input & select values
-  var inputs = d3.select("option");
-  console.log(inputs.value);
+  clear_table();
+  // use helper functions to retrieve inputs
+  var shape = monitor_shape();
+  var input_obj =  retrieve_inputs();
+  // add shape into input obj
+  input_obj['shape'] = shape;
+  // Declare filter variables
+  var filtdate = input_obj['datetime'];
+  var filtcity = input_obj['city'];
+  var filtstate = input_obj['state'];
+  var filtcountry = input_obj['country'];
+  var filtshape = input_obj['shape'];
 
   // Filter the tableData for an exact match for the criteria provided
-  var filteredData = tableData.filter(event => event.datetime === inputValue);
-  // Return the filtered Data
-  console.log(filteredData);
+  var filteredData = tableData.filter(event => {
+    // If all values are undefined ("") filter no events
+    // This is almost certainly a suboptimal solution
+    if (filtdate === "") {var _date = event.datetime}
+    else {var _date = filtdate}
+    if (filtcity === "") {var _city = event.city}
+    else {var _city = filtcity}
+    if (filtstate === "") {var _state = event.state}
+    else {var _state = filtstate}
+    if (filtcountry === "") {var _country = event.country}
+    else {var _country = filtcountry}
+    if (filtshape === "") {var _shape = event.shape}
+    else {var _shape = filtshape}
+    if (_date === event.datetime &&
+        _city === event.city &&
+        _state === event.state &&
+        _country === event.country &&
+        _shape === event.shape) {return event};
+});
   // Loop over the filteredData and add <tr> and <td> elements to hold data
   var tableBody = d3.select("#ufo-table").select("tbody");
   filteredData.forEach(object => {
-    tableBody.append("tr"),
-    tableBody.append("td").text(object.datetime),
-    tableBody.append("td").text(object.city.toUpperCase()),
-    tableBody.append("td").text(object.state.toUpperCase()),
-    tableBody.append("td").text(object.country.toUpperCase()),
-    tableBody.append("td").text(object.shape),
-    tableBody.append("td").text(object.durationMinutes),
-    tableBody.append("td").text(object.comments)
+    tableBody.append("tr").attr("class","temp-tr"),
+    tableBody.append("td").text(object.datetime).attr("class","temp-tr"),
+    tableBody.append("td").text(object.city.toUpperCase()).attr("class","temp-tr"),
+    tableBody.append("td").text(object.state.toUpperCase()).attr("class","temp-tr"),
+    tableBody.append("td").text(object.country.toUpperCase()).attr("class","temp-tr"),
+    tableBody.append("td").text(object.shape).attr("class","temp-tr"),
+    tableBody.append("td").text(object.durationMinutes).attr("class","temp-tr"),
+    tableBody.append("td").text(object.comments).attr("class","temp-tr")
   });
-  var newRow = tableBody.append("tr")
 });
